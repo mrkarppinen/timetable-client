03ddd761fbf93e4efe8d2a673bbe9a97b9330ad1
Jussi Karppinen <jkarppin@Jussi-MacBook-Pro.local>
2018-10-11 13:52:08 +0300

    query updated

--- test/test.js
+++ test/test.js
@@ -15,13 +15,15 @@ describe('timetable', function (){
 
 
   it('load', function (){
-    let times = new Timetable(body.data).sort().toJSON();
+    let times = new Timetable(body.data, '1234').sort().toJSON();
 
     times.timetable[0].time.should.equal(23160);
     times.timetable[0].lines[0].should.equal('66K');
 
     times.timetable[15].time.should.equal(104700);
     times.timetable[15].lines[0].should.equal('67N');
+
+    times.id.should.equal('1234');
   });
 
 });
